bf1b22fee229c4f42c821d59238e43246caabe11
Sam Rahimi <samrahimi420@gmail.com>
2019-10-15 18:08:51 -0400

    control bar WIP

--- server/client/video.js
+++ server/client/video.js
@@ -1,5 +1,5 @@
-const serverUrl='' //if running on a different host then the front end, otherwise leave blank
-const defaultChannelUrl='/channel/TrueLifeTV'
+const serverUrl='http://samrahimi.com' //if running on a different host then the front end, otherwise leave blank
+const defaultChannelUrl='http://samrahimi.com/channel/TrueLifeTV'
 
 
 //Get your own key by going to google cloud api console, youtube API v3. The one below works only for truelife sites
